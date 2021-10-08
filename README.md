cd3a56eeac9e88be1b29b473d4a7763d2fb8878c
HugoMichard <hugomichard@yahoo.com>
2021-10-08 12:56:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## Mission To Mars
+
+This test was provided by Suricats. The test tasks are described in the MissionToMars.md file.
+
+### Installation
+
+This test is solved with Python3.
+
+Prerequisites :
+- Have Python3 installed (https://www.python.org/downloads/)
+- Have pip installed
+
+Install the project by running the following commands in your terminal :
+```
+git clone https://github.com/HugoMichard/suricats-assignement.git suricats-assignement
+cd suricats-assignement
+pip install -r requirements.txt
+```
+
+### Architecture
+
+Main.py file contains the solution to the test and the test.py file contains the tests 
+implemented to check the code robustness.
+
+### Tests
+
+Tests are in the test.py file.
---
+++ test.py
@@ -0,0 +1,44 @@
+import unittest
+from main import Rover
+
+
+class Tests(unittest.TestCase):
+    def test_move_R(self):
+        r = Rover()
+        r.move("R")
+        self.assertEqual(r.facing, "E")
+
+    def test_move_F(self):
+        r = Rover()
+        r.move("F")
+        self.assertEqual(r.position[0], 0)
+        self.assertEqual(r.position[1], 1)
+
+    def test_run_L(self):
+        r = Rover()
+        r.run("LL")
+        self.assertEqual(r.get_communication(), "0 0 S")
+
+    def test_run_B(self):
+        r = Rover()
+        r.run("BBBBBBBBB")
+        self.assertEqual(r.get_communication(), "0 -9 N")
+
+    def test_full_run(self):
+        r = Rover()
+        r.run("LBLFRRLBRLFBRL\nLBBBFRLBLLLFR\nLBRLBLLLL")
+        self.assertEqual(r.get_communication(), "2 -1 W\n1 2 N\n3 2 W")
+
+    def test_assignement_part_1(self):
+        r = Rover()
+        r.run("RFFFFLFFFF")
+        self.assertEqual(r.get_communication(), "4 4 N")
+
+    def test_assignement_full(self):
+        r = Rover()
+        r.run("RFFFFLFFFF\nLFFL")
+        self.assertEqual(r.get_communication(), "4 4 N\n2 4 S")
+
+
+if __name__ == '__main__':
+    unittest.main()
